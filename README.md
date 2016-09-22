b2191063428f373c3a2235b8fd19998bb65a0db8
amoshydra <amoswwj@gmail.com>
2016-09-23 03:06:05 +0800

    Update instruction to get LAPI Key

--- README.md
+++ README.md
@@ -10,8 +10,9 @@ npm install && npm start
 A LAPI key is required to run this project.
 Follow the steps below to set up your LAPI key
 
-1. rename `data/config.sample.js` to `data/config.js`
-2. replace `var LAPI_Key = "1a2B3c4D5e6F7g8H9i0J1";` with your own LAPI key
+1. locate `data/config.sample.js`
+2. replace `var LAPI_KEY = "1a2B3c4D5e6F7g8H9i0J1";` with your own LAPI key
+3. save the file as `data/config.js`
 
 If you do not have a LAPI key, visit http://ivle.nus.edu.sg/LAPI/default.aspx to get one.
 
--- data/config.sample.js
+++ data/config.sample.js
@@ -2,11 +2,12 @@
     A LAPI key is required to run this project.
     Follow the steps below to set up your LAPI key
 
-    1. rename `data/config.sample.js` to `data/config.js`
-    2. replace `var LAPI_Key = "1a2B3c4D5e6F7g8H9i0J1";` with your own LAPI key
+    1. locate `data/config.sample.js`
+    2. replace `var LAPI_KEY = "1a2B3c4D5e6F7g8H9i0J1";` with your own LAPI key
+    3. save the file as `data/config.js`
 
     If you do not have a LAPI key, visit http://ivle.nus.edu.sg/LAPI/default.aspx to get one.
 */
 
-var LAPI_Key = "1a2B3c4D5e6F7g8H9i0J1";
-module.exports = LAPI_Key
+var LAPI_KEY = "1a2B3c4D5e6F7g8H9i0J1";
+module.exports = LAPI_KEY
