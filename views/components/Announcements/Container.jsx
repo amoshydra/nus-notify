01081696b7a2f5850230e5ac1c3fc818b9a376a8
Amos Wong <amoswwj@gmail.com>
2016-10-09 20:04:02 +0800

    Initial setup for react

---
+++ views/components/Announcements/Container.jsx
@@ -0,0 +1,15 @@
+'use babel';
+
+import React, { Component } from 'react';
+import Annoncement from './Item';
+
+export default class Container extends Component {
+
+  render() {
+    return (
+      <div>
+        <Annoncement />
+      </div>
+    );
+  }
+}
---
+++ views/components/Announcements/Item.jsx
@@ -0,0 +1,14 @@
+'use babel';
+
+import React, { Component } from 'react';
+
+export default class Announcement extends Component {
+
+  render() {
+    return (
+      <div>
+        <p> YES!! </p>
+      </div>
+    );
+  }
+}
