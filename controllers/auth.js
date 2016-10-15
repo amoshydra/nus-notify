7d1b839804eb0ac28d812043f78f8a89b47ad644
Amos Wong <amoswwj@gmail.com>
2016-10-16 07:41:25 +0800

    Style ivle login

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ const AuthWindows = {
   bindEventListener: function bindEventListener() {
     const self = this;
     this.authWindow.once('ready-to-show', () => {
-      self.authWindow.webContents.insertCSS('body > table {display: none;}');
+      self.authWindow.webContents.insertCSS(authCSS);
       self.authWindow.show();
     });
 
@@ -63,12 +63,12 @@ const AuthWindows = {
 
   loadAuthPage: function loadAuthPage() {
     this.authWindow.loadURL(this.authUrlPath);
+    this.authWindow.setMenu(null);
   }
 };
 
 function handleToken(event, urlStr) {
   const token = getAuthToken(urlStr);
-
   if (token) {
     Storage.user.set('user.authToken', token);
 
@@ -81,6 +81,7 @@ function handleToken(event, urlStr) {
   } else if (urlStr !== AuthWindows.authUrlPath) {
     AuthWindows.loadAuthPage();
   }
+  AuthWindows.authWindow.webContents.insertCSS(authCSS);
 }
 
 function getAuthToken(urlStr) {
@@ -97,3 +98,100 @@ function getAuthToken(urlStr) {
 
 
 module.exports = Auth;
+
+
+const authCSS = `
+
+  * {
+    font-family: sans-serif !important;
+  }
+
+  body {
+    padding-top: 20px;
+  }
+
+  body > table,
+  #frm > table > tbody > tr:nth-child(1) > td:nth-child(1),
+  #frm > table > tbody > tr:nth-child(1) > td:nth-child(2),
+  #frm > table > tbody > tr:nth-child(2) > td:nth-child(1),
+  #frm > table > tbody > tr:nth-child(2) > td:nth-child(2) {
+    display: none;
+  }
+
+  #frm > table {
+    width: 100%;
+  }
+
+  #frm > table > tbody > tr > td {
+    text-align: center;
+    padding: 4px 10px;
+  }
+
+  #password, #userid {
+    width: 100%;
+    padding: 6px 10px;
+    box-sizing: border-box;
+    text-align: center;
+    margin-bottom: 12px;
+    border: none;
+    border-bottom: 1px solid rgba(0,0,0,0.2);
+    letter-spacing: 0.8px;
+  } #password:focus, #userid:focus {
+    outline: none;
+    border-bottom: 1.8px solid rgba(30,90,150,1);
+  }
+
+  #frm > table > tbody > tr:nth-child(1) > td:nth-child(3)::before,
+  #frm > table > tbody > tr:nth-child(2) > td:nth-child(3)::before {
+    position: absolute;
+    opacity: 0.5;
+    font-family: sans-serif;
+    font-size: 0.9em;
+    left: 35px;
+    padding-top: 4px;
+  }
+
+  #frm > table > tbody > tr:nth-child(3) > td {
+    padding: 0;
+    font-size: 0.9em;
+    color: rgba(255,0,0,0.7);
+    top: -10px;
+    position: relative;
+  }
+  #frm > table > tbody > tr:nth-child(3) > td::before {
+    position: absolute;
+    text-align: center;
+    width: 110px;
+    left: 0;
+    right: 0;
+    margin: 50px auto;
+    background-color: rgba(30,90,150,1);
+    color: white;
+    padding: 10p;
+    font-size: 1.25em;
+    padding: 10px 10px 8px 10px;
+    content: "LOGIN";
+    font-weight: 500;
+    letter-spacing: 1.2px;
+    font-family: sans-serif;
+  }
+
+  #frm > table > tbody > tr:nth-child(1) > td:nth-child(3)::before {
+    content: "Login";
+  }
+  #frm > table > tbody > tr:nth-child(2) > td:nth-child(3)::before {
+    content: "Password";
+  }
+
+  #loginimg1 {
+    margin-bottom: 40px;
+    opacity: 0;
+    position: fixed;
+    left: 0;
+    right: 0;
+    margin: auto;
+    top: 160px;
+    width: 130px;
+  }
+
+`;
